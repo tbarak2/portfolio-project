1d3625d356aa672eb9d50e9de4a4331f15f00722
roi_barak <barakroy178@gmail.com>
2019-05-14 22:32:20 +0300

    Currently create error so it is in comment

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -22,5 +22,5 @@ import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home' ),
-    path('blog/', ('blog.url') ),
+    #path('blog/', ('blog.url') ),
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
